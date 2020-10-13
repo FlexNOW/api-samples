e5cdbe4a5cb06d591d2cc5358b730689d210f8fa
Alex Stiff <astiff@flextrade.com>
2020-10-13 11:58:34 +0100

    Add execution type

--- execution-report/execs.py
+++ execution-report/execs.py
@@ -5,3 +5,4 @@ class StreetOrderExec:
         self.price = exec_info.get("price")
         self.last_market = exec_info.get("lastMarket")
         self.transaction_time = exec_info.get("transactionTime")
+        self.type = exec_info.get("executionType")
